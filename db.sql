6455e5825aaec27778f1f8d90f9839933693d8a5
Rob Wilson <roobert@gmail.com>
2013-12-15 14:57:27 +0000

    updated

--- db.sql
+++ db.sql
@@ -10,6 +10,7 @@ CREATE TABLE network (
 CREATE TABLE host (
   id       SERIAL PRIMARY KEY,
   name     TEXT NOT NULL UNIQUE,
+  network  INTEGER REFERENCES network(id),
   CHECK (name ~* '^(([a-zA-Z]|[a-zA-Z][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z]|[A-Za-z][A-Za-z0-9\-]*[A-Za-z0-9])$')
 );
 
@@ -18,42 +19,66 @@ CREATE TABLE host (
 CREATE TABLE switch (
   id             SERIAL PRIMARY KEY,
   host           INTEGER REFERENCES host(id),
-  network        INTEGER REFERENCES network(id),
   stack_position INTEGER NOT NULL,
   CHECK (stack_position > 0 AND stack_position < 1024)
+);
+
+-- switch interfaces
+
+CREATE TABLE switch_interface (
+  id            SERIAL PRIMARY KEY,
+  switch        INTEGER REFERENCES switch(id),
+  description   TEXT
 );
 
 CREATE TYPE trunk_mode  AS ENUM ('trunk', '');
 CREATE TYPE admin_state AS ENUM ('enabled', 'disabled');
 CREATE TYPE port_state  AS ENUM ('disconnected', 'down', 'up');
 
-CREATE TABLE switch_interface (
-  id           SERIAL PRIMARY KEY,
-  switch       INTEGER REFERENCES switch(id),
-  description  TEXT,
-  port_group   TEXT,
-  speed        INTEGER,
-  port         INTEGER NOT NULL,
-  duplex       INTEGER,
-  state        port_state,
-  admin_state  admin_state,
-  trunk_mode   trunk_mode,
+CREATE TABLE switch_interface_ethernet (
+  id            SERIAL PRIMARY KEY,
+  interface     INTEGER REFERENCES switch_interface(id),
+  port          INTEGER NOT NULL,
+  channel_group TEXT,
+  speed         INTEGER,
+  duplex        INTEGER,
+  state         port_state,
+  admin_state   admin_state,
   CHECK (port > 0 and port < 1024),
   CHECK (duplex % 10 = 0)
 );
 
+-- switch interface attributes
+
+CREATE TYPE vlan_mode AS ENUM ('add', 'remove');
+
 CREATE TABLE vlan (
-  id   SERIAL PRIMARY KEY,
-  port INTEGER REFERENCES switch_interface(id),
-  vlan INTEGER NOT NULL,
-  CHECK (vlan > 0 AND vlan < 4064)
+  id        SERIAL PRIMARY KEY,
+  interface INTEGER REFERENCES switch_interface_ethernet(id),
+  -- FIXME: add constraint
+  vlan      INTEGER[] NOT NULL,
+  mode      vlan_mode NOT NULL
+);
+
+-- currently we dont care about actual switchport params so no checks...
+CREATE TABLE switchport (
+  id        SERIAL PRIMARY KEY,
+  interface INTEGER REFERENCES switch_interface_ethernet(id),
+  params    TEXT
+);
+
+CREATE TYPE portfast_mode AS ENUM ('portfast', 'disable');
+
+CREATE TABLE spanning_tree (
+  id        SERIAL PRIMARY KEY,
+  interface INTEGER REFERENCES switch_interface_ethernet(id),
+  mode      portfast_mode
 );
 
 -- machines
 
 CREATE TABLE machine (
   id       SERIAL PRIMARY KEY,
-  network  INTEGER REFERENCES network(id),
   host     INTEGER REFERENCES host(id)
 );
 
@@ -64,6 +89,8 @@ CREATE TABLE machine_interface (
   mac       MACADDR NOT NULL UNIQUE,
   CHECK (name ~* '^[a-z]+[0-9]+(:[0-9]+)?$')
 );
+
+-- machine interface related
 
 CREATE TABLE ip (
   id   SERIAL PRIMARY KEY,
